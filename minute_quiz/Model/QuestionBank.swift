5a22ceddb3ccaac25596b946e2f86416778a28df
pulok <mdmunifhasan8520@gmail.com>
2018-07-29 03:13:08 +0600

    Complete the question bank model

--- minute_quiz/Model/QuestionBank.swift
+++ minute_quiz/Model/QuestionBank.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class QuestionBank {
+    
+    //create an array of question object
+    var list = [Question]()
+    
+    init() {
+        //creating a quiz item and appending it to the list
+        let item = Question(image: "Diego Maradona_A", text: "Is It Madona?", correctAnswer: false)
+        
+        //add the Questin to the list of question
+        list.append(item)
+        
+        list.append(Question(image: "pele_A", text: "Here is pele...", correctAnswer: true))
+        list.append(Question(image: "David Beckham_A", text: "David Bhuiya!...", correctAnswer: false))
+        list.append(Question(image: "Socrates_A", text: "Yes Socrates ", correctAnswer: true))
+        list.append(Question(image: "Roberto Carlos_A", text: "I think ronaldo?", correctAnswer: false))
+        
+    }
+}
